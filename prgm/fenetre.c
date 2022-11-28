018d8d9b4b4ab2e8fa9a3670dc8bbaa3526f28ec
toussain <nolan.toussaint77@gmail.com>
2022-11-28 16:15:55 +0100

    update

--- prgm/fenetre.c
+++ prgm/fenetre.c
@@ -6,6 +6,11 @@ int main(void){
 	InitialiserGraphique();
     CreerFenetre(250,100,1350,850);
     ChargerImageFond("../img/fond.png");
+    ChargerImage("../img/start_button.png",550,60,0,0,300,300);
+	ChargerImageFond("../img/fond2.png");
+	ChargerImage("../img/difficulty_levels.png",550,200,0,130,230,90);
+	ChargerImage("../img/difficulty_levels.png",550,350,274,130,230,90);
+	ChargerImage("../img/difficulty_levels.png",550,500,272,270,230,90);
 	Touche();
     FermerGraphique();
     return EXIT_SUCCESS;
